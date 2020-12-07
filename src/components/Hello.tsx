0106ac991fc9e6d237a22271523371e763194dbb
wangmiaomiao <13844997353@163.com>
2020-12-07 11:28:33 +0800

    初始化

---
+++ src/components/Hello.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Button } from 'antd'
+interface Greeting {
+  name: string
+}
+const Hello = (props: Greeting) => <Button>HELLO {props.name}</Button>
+export default Hello
